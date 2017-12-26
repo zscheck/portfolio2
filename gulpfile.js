857c7c4fd9554b3c981f1f4644de1a7a895659ff
Zach Scheck <zach.scheck@gmail.com>
2017-12-26 12:30:27 -0800

    package.json change

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const nodemon = require('gulp-nodemon');
+// const gulp = require('gulp');
+// const uglify = require('gulp-uglify');
+// const concat = require('gulp-concat');
+// const nodemon = require('gulp-nodemon');
 
-gulp.task('build:js', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(uglify())
-        .pipe(concat('bundle.min.js'))
-        .pipe(gulp.dest('dist'))
-});
+// gulp.task('build:js', function() {
+//     return gulp.src('src/js/*.js')
+//         .pipe(uglify())
+//         .pipe(concat('bundle.min.js'))
+//         .pipe(gulp.dest('dist'))
+// });
 
-gulp.task('build:css', function() {
-    return gulp.src('src/css/*.css')
-        .pipe(concat('bundle.min.css'))
-        .pipe(gulp.dest('dist'))
-});
+// gulp.task('build:css', function() {
+//     return gulp.src('src/css/*.css')
+//         .pipe(concat('bundle.min.css'))
+//         .pipe(gulp.dest('dist'))
+// });
+
+// gulp.task('copy', function() {
+//     return gulp.src('src/assets/*.png')
+//         .pipe(gulp.dest('dist/assets'))
+// });
