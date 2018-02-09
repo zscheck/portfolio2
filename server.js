dec37d6e524e5a1944b968079444c1f42ff6d730
Zach Scheck <zach.scheck@gmail.com>
2018-02-09 15:35:21 -0800

    new server routes, and mobile background fix

--- server.js
+++ server.js
@@ -20,19 +20,37 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/thanks', (req, res) => {
-    console.log('res', res.body);
-    console.log('req', req.body.name, req.body.message);
-    const msg = {
-        to: 'zach.scheck@gmail.com',
-        from: 'zach.scheck@gmail.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'Name: '+req.body.name + 'Email: ' + req.body.email + 'Message: '+ req.body.message,
-        html: '<strong>Name: </strong>'+req.body.name + '<br><strong>Email: </strong>' + req.body.email + '<br><strong>Message: </strong>'+ req.body.message,
-    };
-    sgMail.send(msg);
-    res.render('thanks', {contact: req.body})
+app.get('/jobleads', (req, res) => {
+    res.render('jobleads');
 });
+
+app.get('/deliciousdeliveries', (req, res) => {
+    res.render('deliciousDeliveries');
+});
+
+app.get('/hangman', (req, res) => {
+    res.render('hangman');
+});
+
+app.get('/mlb-tailgates', (req, res) => {
+    res.render('mlbTailgates');
+});
+
+// NO LONGER RELEVENT, BUT STILL AN OPTION IF I GO BACK TO SENDGRID
+//
+// app.post('/thanks', (req, res) => {
+//     console.log('res', res.body);
+//     console.log('req', req.body.name, req.body.message);
+//     const msg = {
+//         to: 'zach.scheck@gmail.com',
+//         from: 'zach.scheck@gmail.com',
+//         subject: 'Sending with SendGrid is Fun',
+//         text: 'Name: '+req.body.name + 'Email: ' + req.body.email + 'Message: '+ req.body.message,
+//         html: '<strong>Name: </strong>'+req.body.name + '<br><strong>Email: </strong>' + req.body.email + '<br><strong>Message: </strong>'+ req.body.message,
+//     };
+//     sgMail.send(msg);
+//     res.render('thanks', {contact: req.body})
+// });
 
 app.set("port", (process.env.PORT || 3080));
 
